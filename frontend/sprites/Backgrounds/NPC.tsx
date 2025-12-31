dd02e42218aaaf33a269e614c70a5ba9ec977244
Buggy616 <jayleeoglesby@gmail.com>
2025-12-31 17:02:08 -0500

    whateves

---
+++ frontend/sprites/Backgrounds/NPC.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="NPC" tilewidth="66" tileheight="84" tilecount="3" columns="3">
+ <image source="../npcSheet.png" width="200" height="84"/>
+</tileset>
---
+++ frontend/sprites/Backgrounds/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="collision" tilewidth="32" tileheight="30" tilecount="1" columns="1">
+ <image source="../collsion.png" trans="ff00ff" width="32" height="30"/>
+</tileset>
